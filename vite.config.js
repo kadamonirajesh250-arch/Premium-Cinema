e9505a0e6efb6030097d3277f974193e77e5cc7d
kadam <kadam@example.com>
2026-05-30 19:39:16 +0530

    fix: standardize Vite configuration for Vercel deployment and backup offline-demo

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    open: '/react.html'
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'react.html'
-      }
-    }
-  }
 })
